b8b024ccbd790d6d585a892c04c4387ba52f0d6d
kolszlegier41 <kolszlegier41@gmail.com>
2021-08-11 14:29:13 +0200

    feat(main-page): header add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 import {AppComponent} from './app.component';
 import {AuthPageComponent} from './modules/auth-page/auth-page.component';
 import {AppRoutingModule} from "./routing-module";
 import {ExistingUserPageComponent} from './modules/existing-user-page/existing-user-page.component';
+import {MainPageComponent} from './modules/main-page/main-page.component';
+import {HeaderComponent} from './modules/main-page/header/header.component';
+import {MatIconModule} from "@angular/material/icon";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthPageComponent,
-    ExistingUserPageComponent
+    ExistingUserPageComponent,
+    MainPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +30,9 @@ import {ExistingUserPageComponent} from './modules/existing-user-page/existing-u
     MatInputModule,
     FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
