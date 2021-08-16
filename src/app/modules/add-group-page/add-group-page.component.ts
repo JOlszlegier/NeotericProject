91bee569bae6201f5a852d1bc7bbe1930fe8b029
kolszlegier41 <kolszlegier41@gmail.com>
2021-08-16 08:48:09 +0200

    feat(new-group): add remove and add new human with using formArray

--- src/app/modules/add-group-page/add-group-page.component.ts
+++ src/app/modules/add-group-page/add-group-page.component.ts
@@ -1,12 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-add-group-page',
   templateUrl: './add-group-page.component.html',
   styleUrls: ['./add-group-page.component.scss']
 })
-export class AddGroupPageComponent {
+export class AddGroupPageComponent implements OnInit {
   groupName: string = '';
+  groupNewPeople: FormArray = new FormArray([])
 
+  addNewPeople() {
+    this.groupNewPeople.controls.push(new FormControl(''));
+  }
 
+  removePeople(index: number) {
+    this.groupNewPeople.removeAt(index);
+  }
+
+  ngOnInit() {
+    this.groupNewPeople.controls.push(new FormControl(''))
+    this.groupNewPeople.controls.push(new FormControl(''))
+    this.groupNewPeople.controls.push(new FormControl(''))
+  }
 }
