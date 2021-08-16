c534fc14a526f9d25a8df0b2ec83dc2567cd9825
kolszlegier41 <kolszlegier41@gmail.com>
2021-08-16 16:46:23 +0200

    feat(center): mid-box with 3 separate mini boxes add, change text color depend on  value

---
+++ src/app/modules/main-page/center-box/cash-status/cash-status.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-cash-status',
+  templateUrl: './cash-status.component.html',
+  styleUrls: ['./cash-status.component.scss']
+})
+export class CashStatusComponent {
+  moneyTest: number[] = [-10, 2, 25]
+
+}
